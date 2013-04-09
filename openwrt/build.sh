072ccc03f10469d6a81862ad7d0a9d563d586eaf
Markus Stenberg <markus.stenberg@iki.fi>
2013-04-09 14:38:31 +0300

    Added some missing scripts/feeds install's + updated UML config. Hopefully it is now ~1-1 to what I had before in my custom env.

--- openwrt/build.sh
+++ openwrt/build.sh
@@ -8,15 +8,31 @@
 # Copyright (c) 2013 cisco Systems, Inc.
 #
 # Created:       Mon Apr  8 20:11:27 2013 mstenber
-# Last modified: Mon Apr  8 20:17:34 2013 mstenber
-# Edit time:     5 min
+# Last modified: Tue Apr  9 14:34:32 2013 mstenber
+# Edit time:     13 min
 #
 
+HNET_PACKAGES="hnet netkit"
+EXTRA_PACKAGES="luci rsync strace tcpdump"
+
+install_packages () {
+    for PACKAGE in $*
+    do
+        echo "Installing package $PACKAGE"
+        (cd dist && scripts/feeds install $PACKAGE)
+    done
+}
+
 # Link our 'config' to dist/.config if necessary
-if [ ! -L dist/.config ]
+if [ -f dist/.config ]
+then
+    echo "dist/.config is non-symlink! Something is horribly wrong."
+    exit 1
+elif [ ! -L dist/.config ]
 then 
     ln -s ../config dist/.config
 fi
+
 
 # Link files/ to dist/
 if [ ! -L dist/files ]
@@ -33,11 +49,9 @@ if [ ! -f dist/feeds.conf ]
 then
     cp feeds.conf dist/
     echo "src-link hnet "`pwd`/feed >> dist/feeds.conf
-    (cd dist \
-        && scripts/feeds update \
-        && scripts/feeds install hnet \
-        && scripts/feeds install netkit \
-        )
+    (cd dist && scripts/feeds update)
+    install_packages $HNET_PACKAGES
+    install_packages $EXTRA_PACKAGES
 fi
 
 make -C dist -j 9
