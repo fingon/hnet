ff8b2a39a158b584c6138c8822ee2c9a087ae122
Markus Stenberg <markus.stenberg@iki.fi>
2013-06-27 09:58:06 +0300

    Added packaging bits required by luajit.

--- openwrt/build.sh
+++ openwrt/build.sh
@@ -8,12 +8,12 @@
 # Copyright (c) 2013 cisco Systems, Inc.
 #
 # Created:       Mon Apr  8 20:11:27 2013 mstenber
-# Last modified: Tue Jun 25 10:53:42 2013 mstenber
+# Last modified: Thu Jun 27 09:57:32 2013 mstenber
 # Edit time:     27 min
 #
 
 HNET_PACKAGES="hnet netkit"
-EXTRA_PACKAGES="luci rsync strace tcpdump procps valgrind"
+EXTRA_PACKAGES="luci rsync strace tcpdump procps valgrind luajit"
 
 install_packages () {
     for PACKAGE in $*
