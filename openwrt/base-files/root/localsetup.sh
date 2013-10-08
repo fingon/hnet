2c48b56216f4792aebbe16c6d3835e0170d3a057
Markus Stenberg <markus.stenberg@iki.fi>
2013-10-08 17:10:01 +0300

    Some initial work on a Buffalo config. So far no luck getting it working, though.

--- openwrt/base-files/root/localsetup.sh
+++ openwrt/base-files/root/localsetup.sh
@@ -5,33 +5,6 @@ then
     mv ${NETWORK}.new ${NETWORK}
 fi
 
-# Disable unneeded services
-echo " * Disabling services"
-for SERVICE in \
-  babeld \
-  bird4 \
-  cron \
-  dhclient \
-  dhclient6 \
-  dhcpd \
-  dhcpd6 \
-  dibbler-client \
-  dibbler-server \
-  dnsmasq \
-  radvd \
-  telnet 
-do
-    if [ -f /etc/init.d/$SERVICE ]
-    then
-        echo "  - disabling $SERVICE"
-        /etc/init.d/$SERVICE disable
-    fi
-done  
-
-# Disable firewalling altogether
-# (Room for future development, sigh)
-/etc/init.d/firewall disable
-
 HOSTNAME=inner
 echo " * Setting hostname to $HOSTNAME"
 uci set "system.@system[0].hostname=$HOSTNAME"
